b4a6c11e9733e439b29de718c1d2feae16e6438d
BernasPL3 <bernardogamesguidi@gmail.com>
2026-05-30 10:57:40 -0300

    Update MainActivity.kt

--- app/src/main/java/com/alldroid/app/MainActivity.kt
+++ app/src/main/java/com/alldroid/app/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.alldroid.app
 
 import android.os.Bundle
+import android.os.Environment
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val tv = TextView(this)
+        setContentView(tv)
+
+        val roms = RomScanner.scan(
+            Environment.getExternalStorageDirectory()
+        )
+
+        val builder = StringBuilder()
+
+        builder.append("ROMs encontradas: ${roms.size}\n\n")
+
+        roms.forEach {
+
+            builder.append("${it.system}\n")
+            builder.append("${it.name}\n")
+            builder.append("\n")
+        }
+
+        tv.text = builder.toString()
     }
 }
